3c66c54e91f76f3c090dfdbb9315f8c42bd45615
jimitas <nonajun805@gmail.com>
2024-01-05 19:50:57 +0900

    Headタグを追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <title>jimitas(地味に助かる学習コンテンツ)</title>
+        <meta name="description" content="jimitas(地味に助かる学習コンテンツ)" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
