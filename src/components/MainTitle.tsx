4b1ad323729b341005468b22a233201d2ac2b533
jimitas <nonajun805@gmail.com>
2024-01-05 20:17:51 +0900

    MainTitle CSS微調整

--- src/components/MainTitle.tsx
+++ src/components/MainTitle.tsx
@@ -6,7 +6,7 @@ const MainTitle: React.FC = () => {
   return (
     <div>
       <div className="mt-5 flex justify-center items-center">
-        <div>
+        <div className="w-8 sm:w-10 md:w-12 lg:w-14">
           <FontAwesomeIcon className="text-lg sm:text-2xl md:text-3xl lg:text-4xl" icon={faCat} />　
         </div>
         <div className="text-lg sm:text-2xl md:text-3xl lg:text-4xl">
@@ -24,14 +24,14 @@ const MainTitle: React.FC = () => {
               学習
               <rt>がくしゅう</rt>
             </ruby>
-            コンテンツ　
+            コンテンツ
           </div>
           <div className="text-center text-lg sm:text-2xl md:text-3xl lg:text-4xl">
             Jimitas
             <span className="text-sm sm:text-xl md:text-2xl lg:text-3xl">(ジミタス)</span>
           </div>
         </div>
-        <div>
+        <div className="w-8 sm:w-10 md:w-12 lg:w-14 ms-2">
           <FontAwesomeIcon className="text-lg sm:text-2xl md:text-3xl lg:text-4xl" icon={faCat} />
         </div>
       </div>
