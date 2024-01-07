451865e3daefe5a0683b456fa7af7e52ec58ab0d
jimitas <nonajun805@gmail.com>
2024-01-07 21:59:36 +0900

    btnCheck css調整

--- src/components/PutButton/btnCheck.tsx
+++ src/components/PutButton/btnCheck.tsx
@@ -13,8 +13,13 @@ export function BtnCheck(props: BtnCheckProps) {
 
   return (
     <div className="flex flex-wrap justify-center">
-      <button onClick={handleEvent} className={styles.btnQuest}>
-        <FontAwesomeIcon icon={faCheck} className="w-8 h-8"/>
+      <button
+        onClick={handleEvent}
+        className="flex justify-center item-center font-bold m-2 p-2 
+                   w-28 md:w-36 text-sm md:text-base
+                   border-red-300 bg-white border-2 text-red-400  hover:bg-red-500 hover:text-white active:translate-y-1 rounded-lg shadow-lg"
+      >
+        <FontAwesomeIcon icon={faCheck} className="w-4 h-4 md:w-6 md:h-6" />
         {btnText}
       </button>
     </div>
--- src/components/PutSelect/index.jsx
+++ src/components/PutSelect/index.jsx
@@ -6,7 +6,6 @@ export function PutSelect(props) {
       <select
         onChange={handleEvent}
         className="text-center item-center font-bold m-2 p-2 min-w-24 text-base md:text-xl border-green-400 bg-white border-2 rounded-lg shadow-lg"
-        style={{ margin: "0 5px" }}
       >
         {ITEM.map((item) => {
           return (
