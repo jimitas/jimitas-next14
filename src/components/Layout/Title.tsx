b47c33932cbe7deebdf741e4c68f1ad44997d41b
jimitas <nonajun805@gmail.com>
2024-01-07 11:20:11 +0900

    z-indexの調整 ブロックに色付けをする。 タイトルを太字に

--- src/components/Layout/Title.tsx
+++ src/components/Layout/Title.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Title = (props: { title: string }) => {
   return (
-    <div id="content-title" className="mt-5 text-2xl md:text-3xl lg:text-4xl text-center text-bold">
+    <div id="content-title" className="mt-5 text-2xl md:text-3xl lg:text-4xl text-center font-bold">
       {props.title}
     </div>
   );
--- todoMemo.md
+++ todoMemo.md
@@ -2,3 +2,9 @@
 ファビコン作成
 
 PutButton群の Cssをtailwinndで記述
+
+ブロックコンポーネントの配置　btnSpace使わなくてもできる。
+
+z-indexの上下関係
+
+tableの色付
